5ea6ebc69a7d53eafc216a5b87426f9598319e29
luo_yifan <luo_yifan@devide>
2025-02-25 21:08:00 +0800

    Tue Feb 25 21:08:00 CST 2025 inscode

--- ddd-application/target/generated-sources/annotations/com/ddd/applicaiton/converter/UserApplicationConverterImpl.java
+++ ddd-application/target/generated-sources/annotations/com/ddd/applicaiton/converter/UserApplicationConverterImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-02-24T16:23:43+0800",
+    date = "2025-02-25T21:04:32+0800",
     comments = "version: 1.2.0.Final, compiler: Eclipse JDT (IDE) 3.33.0.v20230213-1046, environment: Java 17.0.8 (Private Build)"
 )
 @Component
